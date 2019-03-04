477d63a2200e5d441b04acd07ee068a49826f5c4
Mathilde <mathilde_terleth@hotmail.com>
2019-03-04 17:28:56 +0100

    updated

--- ss_sym.py
+++ ss_sym.py
@@ -5,13 +5,16 @@ Created on Mon Mar  4 17:00:04 2019
 @author: mathi
 """
 from Constants import *
-import scipy as sp
+import control.matlab as control
+import numpy as np
+from math import *
+
 def ss_sym():
     
-    C1 = np.matrix([[-2*muc*c/V0, 0.,0.,0.],
-               [0., (CZadot - 2*muc)*(c/V0), 0., 0.],
-               [0.,0.,-(c/V0), 0.],
-               [0., Cmadot*(c/V0), 0., -2*muc*KY2*(c/V0)**2]])
+    C1 = np.matrix([[-2*muc*(c/V0), 0.,0.,0.],
+                     [0., (CZadot - 2*muc)*(c/V0), 0., 0.],
+                     [0.,0.,-(c/V0), 0.],
+                     [0., Cmadot*(c/V0), 0., -2*muc*KY2*(c/V0)**2]])
 
     C2 = np.matrix([[CXu, CXa, CZ0, CXq*(c/V0)],
                     [CZu, CZa, -CX0, (CZq + 2*muc)*(c/V0)],
@@ -26,7 +29,10 @@ def ss_sym():
     
     Ds =np.matrix([[0.],[0.],[0.],[0.]])
     
-    As = -np.linalg.inv(C1)*C2
-    Bs = -np.linalg.inv(C1)*C3
+    As = np.dot(-np.linalg.inv(C1),C2)
+    Bs = np.dot(-np.linalg.inv(C1),C3)
     
-    sys1 = sp.ss(As,Bs,Cs,Ds)
+    sys1 = control.ss(As,Bs,Cs,Ds)
+    
+    print(sys1)
+    
