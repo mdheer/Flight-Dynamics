133a142447d854dbc3bbd8c7148d823562bff0ad
Willem <willem.volker@gmail.com>
2019-03-06 09:40:31 +0100

    Finished SS-model (asymmetric motion)

--- asymm_SS.py
+++ asymm_SS.py
@@ -6,27 +6,36 @@ Created on Mon Mar  4 15:50:11 2019
 @author: willemvolker
 """
 from Constants import *
-import numpy as np
 import numpy.linalg as la
+import control.matlab as c
+import matplotlib.pyplot as plt
 
 mub = 1 #?
 Db = 1 #?
-V = 1 #?
 
-C1 = np.matrix([[(CYbdot-2*mub)*b/V, 0, 0, 0],\
-                 [0, -1/2*b/V, 0, 0],\
-                 [0, 0, -4*mub*KX2*b/V, 4*mub*KXZ*b/V],\
-                 [Cnbdot*b/V, 0, 4*mub*KXZ*b/V, -4*mub*KZ2*b/V]])
+C1 = [[(CYbdot-2*mub)*b/V0, 0, 0, 0],\
+                 [0, -1/2*b/V0, 0, 0],\
+                 [0, 0, -4*mub*KX2*b/V0, 4*mub*KXZ*b/V0],\
+                 [Cnbdot*b/V0, 0, 4*mub*KXZ*b/V0, -4*mub*KZ2*b/V0]]
 
-C2 = np.matrix([[CYb+(CYbdot-2*mub)*Db, CL, CYp, CYr-4*mub],\
+C2 = [[CYb+(CYbdot-2*mub)*Db, CL, CYp, CYr-4*mub],\
                  [0, -1/2*Db, 1, 0],\
                  [Clb, 0, Clp-4*mub*KX2*Db, Clr+4*mub*KXZ*Db],\
-                 [Cnb+Cnbdot*Db, 0, Cnp+4*mub*KXZ*Db, Cnr-4*mub*KZ2*Db]])
+                 [Cnb+Cnbdot*Db, 0, Cnp+4*mub*KXZ*Db, Cnr-4*mub*KZ2*Db]]
 
-C3 = np.matrix([[-CYda, -CYdr],\
+C3 = [[-CYda, -CYdr],\
                 [0, 0],\
                 [-Clda, -Cldr],\
-                [-Cnda, -Cndr]])
+                [-Cnda, -Cndr]]
 
 A = np.dot(-la.inv(C1), C2)
 B = np.dot(-la.inv(C1), C3)
+C = np.matrix([[1, 1, 1, 1]])
+D = np.matrix([[0, 0]])
+
+sys_as = c.ss(A,B,C,D)
+
+#t = np.arange(0,20.01,0.01)
+#y,t = c.impulse(sys_as, t)
+#
+#plt.plot(t,y)
