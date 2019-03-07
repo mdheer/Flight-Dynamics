ad6e94384bced0469eae576a5a99b7320765dc1e
Willem <willem.volker@gmail.com>
2019-03-07 14:30:03 +0100

    Corrected dimension errors

--- asymm_SS.py
+++ asymm_SS.py
@@ -10,23 +10,25 @@ import numpy.linalg as la
 import control.matlab as c
 import matplotlib.pyplot as plt
 
+#Note: do we use V0??
+
 mub = 1 #?
 Db = 1 #?
 
 C1 = [[(CYbdot-2*mub)*b/V0, 0, 0, 0],\
                  [0, -1/2*b/V0, 0, 0],\
-                 [0, 0, -4*mub*KX2*b/V0, 4*mub*KXZ*b/V0],\
-                 [Cnbdot*b/V0, 0, 4*mub*KXZ*b/V0, -4*mub*KZ2*b/V0]]
+                 [0, 0, -4*mub*KX2*b**2/(2*V0**2), 4*mub*KXZ*b**2/(2*V0**2)],\
+                 [Cnbdot*b/V0, 0, 4*mub*KXZ*b**2/(2*V0**2), -4*mub*KZ2*b**2/(2*V0**2)]]
 
-C2 = [[CYb+(CYbdot-2*mub)*Db, CL, CYp, CYr-4*mub],\
-                 [0, -1/2*Db, 1, 0],\
-                 [Clb, 0, Clp-4*mub*KX2*Db, Clr+4*mub*KXZ*Db],\
-                 [Cnb+Cnbdot*Db, 0, Cnp+4*mub*KXZ*Db, Cnr-4*mub*KZ2*Db]]
+C2 = [[CYb, CL, CYp*b/(2*V0), (CYr-4*mub)*b/(2*V0)],\
+                 [0, 0, b/(2*V0), 0],\
+                 [Clb, 0, Clp*b/(2*V0), Clr*b/(2*V0)],\
+                 [Cnb, 0, Cnp*b/(2*V0), Cnr*b/(2*V0)]]
 
-C3 = [[-CYda, -CYdr],\
+C3 = [[CYda, CYdr],\
                 [0, 0],\
-                [-Clda, -Cldr],\
-                [-Cnda, -Cndr]]
+                [Clda, Cldr],\
+                [Cnda, Cndr]]
 
 A = np.dot(-la.inv(C1), C2)
 B = np.dot(-la.inv(C1), C3)
