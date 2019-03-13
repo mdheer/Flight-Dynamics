83a31a28a854cebef2a99818b390ef2ab7da6bcc
Simone <simoon_veldhuis@yahoo.com>
2019-03-13 10:26:45 +0100

    Update to trim and force curves equations

--- StationaryValues.py
+++ StationaryValues.py
@@ -35,12 +35,12 @@ def Ve_thilde(Ve, Ws, W):
     Ve_red = Ve * math.sqrt(Ws/W)
     return Ve_red
 
-def de_star(Cmde, Cm0, Cma, CNwa, Cmtc, W, rho, Ve_red, Tc, S):
-    de_red = (-1./Cmde)*(Cm0 + (Cma/CNwa)*(W/(0.5 * rho * Ve_red**2 * S)) + Cmtc * Tc)
+def de_star(Cmde, Cm0, Cma, CNwa, Cmtc, W, rho, Ve_red, Tcs, S):
+    de_red = (-1./Cmde)*(Cm0 + (Cma/CNwa)*(W/(0.5 * rho * Ve_red**2 * S)) + Cmtc * Tcs)
     return de_red
 
-def Fe_star(W, xcg, rho, Ve_red, dte):
-    Fe_red = ((de[i] - de[i-1])/(Se[i] - Se[i-1])) * Se[i] * ce * (Vh/V)**2 *((Chd/Cmd) * ((xcg - xnfree)/c) * (W/S) - 0.5 * rho * Ve_red**2 * Chdt * (dte - dte0))
+def Fe_star(Ws, W, Fmeas):
+    Fe_red = Fmeas * (Ws/W)
     return Fe_red    
 
 # Ws = standard aircraft weight
