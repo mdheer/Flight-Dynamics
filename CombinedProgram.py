471cd95942a9959e6769d3dcbf497d2811721ea2
Mathilde <mathilde_terleth@hotmail.com>
2019-03-25 11:13:46 +0100

    change

--- CombinedProgram.py
+++ CombinedProgram.py
@@ -130,8 +130,6 @@ for i in range(len(stat_2_conv[0][1])):
 
 #calculate these values for first cg position (xcg1)
 p_xcg1, rho_xcg1, M_xcg1, T_xcg1, W_xcg1, V_TAS_xcg1, Ve_xcg1, a_xcg1 = StationaryValues(stat_xcg_conv[0][1][0][0], stat_xcg_conv[9][1][0][0], stat_xcg_conv[1][1][0][0]-2, stat_mass(stat_xcg_conv[8][1][0][0]))
-#calculate these values for second cg position (xcg2)
-p_xcg2, rho_xcg2, M_xcg2, T_xcg2, W_xcg2, V_TAS_xcg2, Ve_xcg2, a_xcg2 = StationaryValues(stat_xcg_conv[0][1][1][0], stat_xcg_conv[9][1][1][0], stat_xcg_conv[1][1][1][0]-2, stat_mass(stat_xcg_conv[8][1][1][0]))
 
 """ ============== V. Make the thrust file ============== """
 
@@ -363,16 +361,16 @@ elif len(Alpha2)==5:
 
 """ Reduced elevator deflection vs Angle of attack plot(to determine Cm_alpha) """
 
-#plt.plot(alpha2_sorted, de_red_sorted)
-#plt.gca().invert_yaxis()
-#plt.ylabel('Reduced elevator deflection [degree]')
-#plt.xlabel('Angle of attack [degree]')
-#plt.show()
+plt.plot(alpha2_sorted, de_red_sorted)
+plt.gca().invert_yaxis()
+plt.ylabel('Reduced elevator deflection [degree]')
+plt.xlabel('Angle of attack [degree]')
+plt.show()
 
 
 """############################################### Cm_lpha (Longitudinal stability) #################################"""
 
 Cma = (np.polyfit(alpha2_sorted, de_red_sorted, 1)[0]) * -Cmde
-#print(Cma)
-
+print(Cma)
+print(np.polyfit(alpha2_sorted, de_red_sorted, 1)[0])
 #print(Mlist1)
